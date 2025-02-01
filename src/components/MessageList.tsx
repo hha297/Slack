6e50a1ec2ce89dcb67874c0049785ec6ea870aac
Dang Hoang Ha <ha.dh290702@gmail.com>
2025-02-01 11:50:45 +0200

    Infinite load

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -6,6 +6,7 @@ import { ChannelHero } from './ChannelHero';
 import { useState } from 'react';
 import { useWorkspaceId } from '@/hooks/useWorkspaceId';
 import { useCurrentMember } from '@/features/members/api/useCurrentMember';
+import { Loader } from 'lucide-react';
 interface MessageListProps extends React.HTMLAttributes<HTMLDivElement> {
         memberName?: string;
         memberImage?: string;
@@ -97,6 +98,33 @@ export const MessageList = ({
                                         })}
                                 </div>
                         ))}
+                        <div
+                                className="h-1"
+                                ref={(element) => {
+                                        if (element) {
+                                                const observer = new IntersectionObserver(
+                                                        ([entry]) => {
+                                                                if (entry.isIntersecting && canLoadMore) {
+                                                                        loadMore();
+                                                                }
+                                                        },
+                                                        { threshold: 1 },
+                                                );
+                                                observer.observe(element);
+                                                return () => {
+                                                        observer.disconnect();
+                                                };
+                                        }
+                                }}
+                        />
+                        {isLoadingMore && (
+                                <div className="text-center my-2 relative">
+                                        <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+                                        <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm">
+                                                <Loader className="size-4 animate-spin" />
+                                        </span>
+                                </div>
+                        )}
                         {variants === 'channel' && channelName && channelCreationTime && (
                                 <ChannelHero channelName={channelName} channelCreationTime={channelCreationTime} />
                         )}
